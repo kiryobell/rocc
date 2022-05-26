7c3142868fc91556fd81b997cb6feeef7d129bfd
kiryobell <bellada719@gmail.com>
2022-05-26 10:22:35 +0600

    rocc

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 3,
+        loop: true,
+        // autoplay:true,
+        // autoplayTimeout:2000,
+        // autoplayHoverPause:true,
+        // nav:true,
+        // dots:true,
+    });
+});
